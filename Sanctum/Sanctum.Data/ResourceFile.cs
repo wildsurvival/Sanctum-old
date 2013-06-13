33c91c5cad6e3f6564fbfec9c3057fa9a002073b
Andrew Choate <a_choate@live.com>
2013-06-13 13:42:56 -0400

    Added source headers

--- Sanctum/Sanctum.Data/ResourceFile.cs
+++ Sanctum/Sanctum.Data/ResourceFile.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+Sanctum is a free open-source 2D isometric game engine
+Copyright (C) 2013  Andrew Choate
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+You can contact the author at a_choate@live.com or at the project website
+ */
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
